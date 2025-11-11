7b432d6b97688741c25abefc95973fdc3ebb1726
g4knr <contact@g4knr.co.uk>
2025-11-11 15:49:03 +0000

    Added progress ite

---
+++ src/form/types/items/progress-item.ts
@@ -0,0 +1,20 @@
+import type { BaseItem } from './base-item';
+import type { CardItem, CardParentHierarchy } from './card-item';
+import type { GroupItem, GroupParentHierarchy } from './group-item';
+import type { SetItem, SetParentHierarchy } from './set-item';
+
+export type ProgressParentElement = CardItem | SetItem | GroupItem;
+
+export type ProgressParentHierarchy =
+  | CardParentHierarchy
+  | SetParentHierarchy
+  | GroupParentHierarchy;
+
+/**
+ * Progress Item
+ * @extends BaseItem
+ */
+export interface ProgressItem extends BaseItem {
+  /** Parent hierarchy */
+  parentHierarchy: ProgressParentHierarchy;
+}
