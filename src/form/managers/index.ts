b2df8726fcd4931377ff04a7addd89199498cf39
g4knr <contact@g4knr.co.uk>
2025-11-12 11:39:46 +0000

    Update exports

--- src/form/managers/index.ts
+++ src/form/managers/index.ts
@@ -4,16 +4,14 @@
  * Central export point for all form managers.
  */
 
+export { ButtonManager } from './button-manager';
 export { CardManager } from './card-manager';
+export { ConditionManager } from './condition-manager';
 export { DisplayManager } from './display-manager';
 export { FieldManager } from './field-manager';
+export { FocusManager } from './focus-manager';
 export { GroupManager } from './group-manager';
 export { InputManager } from './input-manager';
 export { NavigationManager } from './navigation-manager';
+export { ProgressManager } from './progress-manager';
 export { SetManager } from './set-manager';
-// export { ValidationManager } from './validation-manager';
-// export { ErrorManager } from './error-manager';
-// export { ConditionManager } from './condition-manager';
-// export { RenderManager } from './render-manager';
-// export { AnimationManager } from './animation-manager';
-// export { AccessibilityManager } from './accessibility-manager';
