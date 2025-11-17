4c5003e45100c35cde274a6e119323a8d96d660f
g4knr <contact@g4knr.co.uk>
2025-11-17 14:54:26 +0000

    Submit events

--- src/form/types/events/submission-events.ts
+++ src/form/types/events/submission-events.ts
@@ -7,8 +7,10 @@
  * Emitted when user clicks submit button
  * Form will validate and gather data before emitting form:submit:started
  */
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface SubmitRequestedEvent {}
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface SubmitStartedEvent {}
 
 export interface SubmitSuccessEvent {
